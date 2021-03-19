2f3c09dbc6207d6443371e9360ad23ebfb10184b
Manav <manavjoshi575@gmail.com>
2021-03-19 11:35:27 +0530

    Refactor for unknown amount valued String

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -11,16 +11,26 @@ public class StringCalculator {
     {
         int sum = 0;
         String[] value;
+
+        // If String is empty set sum = 0.
         if (numbers.isEmpty()){
             sum = 0;
         }
+
+        // Check if String is comma separated.
         else if (numbers.contains(",")){
+
+            // Split the string into individual numbers by comma.
             value = numbers.split(",");
+
+            // Sum up the numbers.
             for (int i = 0; i < value.length; i++){
                 sum =  sum + Integer.parseInt(value[i]);
             }
 
         }
+
+        // String is single valued set sum to given value.
         else {
             sum = Integer.parseInt(numbers);
         }
