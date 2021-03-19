8d6460a32fd7f83011f84d5032398e4eefbe1533
Manav <manavjoshi575@gmail.com>
2021-03-19 11:17:27 +0530

    Pass test for two valued String

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -10,8 +10,13 @@ public class StringCalculator {
     public int Add(String numbers)
     {
         int sum = 0;
+        String[] value;
         if (numbers.isEmpty()){
             sum = 0;
+        }
+        else if (numbers.contains(",")){
+            value = numbers.split(",");
+            sum =  Integer.parseInt(value[0]) + Integer.parseInt(value[1]);
         }
         else {
             sum = Integer.parseInt(numbers);
