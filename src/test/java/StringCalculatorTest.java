bd2680b2cd56c5c0340c6ccdc0ab7f3a87f4b724
Manav <manavjoshi575@gmail.com>
2021-03-19 11:24:21 +0530

    Refactor for two valued String

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -8,28 +8,37 @@
 
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class StringCalculatorTest {
 
+    private StringCalculator stringCalculator;
+
+    // It will initialize before every test.
+    @Before
+    public void setup(){
+        stringCalculator = new StringCalculator();
+    }
+
+    // Empty String test.
     @Test
     public void returnZeroOnEmptyString(){
         String numbers = "";
-        StringCalculator stringCalculator = new StringCalculator();
         Assert.assertEquals(0,stringCalculator.Add(numbers));
     }
 
+    // Single value test.
     @Test
     public void returnSameValueOnOneValueString(){
         String numbers = "1";
-        StringCalculator stringCalculator = new StringCalculator();
         Assert.assertEquals(Integer.parseInt(numbers),stringCalculator.Add(numbers));
     }
 
+    // Two value test.
     @Test
     public void returnSumOfTwoValuesOnTwoValueString(){
         String numbers = "1,2";
-        StringCalculator stringCalculator = new StringCalculator();
         Assert.assertEquals(3,stringCalculator.Add(numbers));
     }
 }
